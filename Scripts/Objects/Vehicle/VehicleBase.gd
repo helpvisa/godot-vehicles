3de4af60a03210a2c826241a9d59164c3d1e2c5f
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2024-02-14 14:41:05 -0500

    changed wheels from resource to a custom class plugin type; initialized basic vehicle suspension

--- Scripts/Objects/Vehicle/VehicleBase.gd
+++ Scripts/Objects/Vehicle/VehicleBase.gd
@@ -1,9 +1,9 @@
-extends Node3D
+extends RigidBody3D
 
 # public / inspector-settable
 @export var engine: Resource
 @export var transmission: Resource
-@export var wheels: Array[Resource]
+@export var wheels: Array[Node] # assign child nodes manually here
 
 var input = {
 	"accel": 0.0,
@@ -13,11 +13,21 @@ var input = {
 
 func _ready():
 	for wheel in wheels:
-		# position wheels based on offsets
-		var instanced_node = wheel.model.instantiate()
-		instanced_node.position += wheel.offset
-		get_node("RigidBody3D").add_child(instanced_node)
+		wheel.initModel()
 
 
-func _process(delta):
-	pass
+func _physics_process(_delta):
+	for wheel in wheels:
+		wheel.updateWheelPosition(self)
+		# calculate suspension forces and apply them to the rigidbody
+		if wheel.isGrounded:
+			var suspensionForce = wheel.calculateSuspensionForce(self)
+			var velocityAtWheel = get_point_velocity(wheel.global_position)
+			velocityAtWheel = velocityAtWheel.project(-global_transform.basis.y)
+			var suspensionDampingForce = velocityAtWheel * -wheel.suspensionDamping
+			var totalAppliedForce = suspensionForce + suspensionDampingForce
+			apply_force(totalAppliedForce, wheel.position)
+
+# custom functions
+func get_point_velocity(point: Vector3) -> Vector3:
+	return linear_velocity + angular_velocity.cross(point - global_position)
---
+++ addons/VehicleComponents/Wheel/wheel_class.gd
@@ -0,0 +1,61 @@
+@tool
+extends Node3D
+
+# public / settable
+@export var offset: Vector3 = Vector3(0, 0, 0)
+@export var pacejkaCurve: Curve
+@export var model: PackedScene
+@export var radius: float = 0.33
+@export var suspensionStrength: float = 24000
+@export var suspensionDamping: float = 1800
+@export var suspensionRange: float = 0.6
+@export var powered: bool = true
+@export var steerable: bool = false
+@export var brakes: bool = true
+@export var maxDriveForce: float = 1400 * 9.8 # m * g; this is a default
+@export_flags_3d_physics var colMask
+
+# local
+var target: Vector3 = Vector3.ZERO
+var previousPosition: Vector3 = Vector3.ZERO
+var previousVelocity: Vector3 = Vector3.ZERO
+var previousForwardVelocity: Vector3 = Vector3.ZERO
+var isGrounded: bool = false
+var currentRPM: float = 0
+var slip: float = 1
+var grip: float = 1
+var suspensionForce: float = 1
+var appliedWeight: float = 1
+var currentDrive: float = 0
+var tractionTorque: float = 0
+var brakingTorque: float = 0
+var angularAcceleration: float = 0
+var angularVelocity: float = 0
+var appliedAcceleration: float = 0
+
+# custom functions
+func initModel():
+	if model:
+		var temp_model = model.instantiate()
+		add_child(temp_model)
+	position = offset + Vector3.DOWN * suspensionRange
+
+func updateWheelPosition(parent):
+	var space = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(parent.to_global(offset + (Vector3.UP * radius) + (Vector3.UP * suspensionRange)),\
+		parent.to_global(offset + (Vector3.DOWN * radius) + (Vector3.DOWN * suspensionRange)),\
+		colMask)
+	var result = space.intersect_ray(query)
+	
+	if result:
+		global_position = result.position + (result.normal * radius)
+		isGrounded = true
+	else:
+		global_position = parent.to_global(offset + Vector3.DOWN * suspensionRange)
+		isGrounded = false
+
+func calculateSuspensionForce(parent) -> Vector3:
+	var suspensionOffset = parent.to_global(position)- parent.to_global(offset + (Vector3.DOWN * radius))
+	var springForce = suspensionOffset * suspensionStrength
+	print(springForce)
+	return springForce
